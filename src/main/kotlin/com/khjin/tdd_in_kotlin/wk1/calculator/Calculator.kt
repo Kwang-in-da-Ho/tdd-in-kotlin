16993c40e5045e346255cdb8efb44e930e229cb1
horilla <khjin1197@gmail.com>
2024-02-21 01:51:10 +0900

    Kata 2 - case 1 : return 0 for empty string

--- src/main/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/Calculator.kt
+++ src/main/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/Calculator.kt
@@ -2,83 +2,8 @@ package org.example.com.khjin.tdd_in_kotlin.wk1.calculator
 
 class Calculator {
 
-    fun add(expression: String): Int {
-        val numList = parse(expression)
-
-        return numList.sum()
-    }
-
-    fun add_v4(expression:String): Int {
-        var result = 0
-
-        if( expression.isEmpty() ) return 0
-        if( expression.length == 1) return expression.toInt()
-
-        val nums = expression.split(",|\\r\\n")
-        for(n in nums)
-            result += n.toInt()
-
-        return result
-    }
-
-    // add version 3
-    fun add_v3(input: String): Int{
-        var result = 0
-
-        if( input.isEmpty() ) return 0
-        if( input.length == 1) return input.toInt()
-
-        var nums = input.split(",")
-        for(n in nums)
-          result += n.toInt()
-
-        return result
-    }
-
-    // add version 2
-    fun add_v2(input: String): Int{
-        return if (input.isEmpty()) 0 else input.toInt()
-    }
-
-    // add version 1
-    fun add_v1(input: String): Int {
+    fun add(expression: String): Int{
         return 0
-    }
-
-
-    /**
-     * parses a string expression to a list of numbers
-     */
-    fun parse(expression: String):List<Int>{
-        val result = ArrayList<Int>()
-        val defaultDelimiter = arrayOf(',', '\n')
-        var numInString = ""
-        var isValid = true
-
-        if( expression.isEmpty() or !expression.last().isDigit() )
-            throw IllegalArgumentException("Invalid Expression");
-
-        for(c in expression.toCharArray()){
-            if( defaultDelimiter.contains(c) ){
-                if( numInString.isNotEmpty() ){
-                    result.add(numInString.toInt())
-                    numInString = ""
-                }else {
-                    isValid = false
-                    break
-                }
-
-            }else if(c.isDigit()){
-                numInString += c
-            }else{
-                isValid = false
-            }
-        }
-
-        if( !isValid ) throw IllegalArgumentException("Invalid Expression");
-        if( numInString.isNotEmpty() ) result.add(numInString.toInt())
-
-        return result
     }
 
 }
--- src/test/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/CalculatorTest.kt
@@ -1,77 +1,14 @@
 package com.khjin.tdd_in_kotlin.wk1.calculator
 
 import org.example.com.khjin.tdd_in_kotlin.wk1.calculator.Calculator
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
 
 class CalculatorTest {
 
     @Test
-    fun shouldReturnZero_whenInputIsEmptyString() {
+    fun shouldReturnZero_whenExpressionIsEmpty() {
         val calculator = Calculator()
         assertEquals(0, calculator.add(""))
-        assertNotEquals(10, calculator.add(""))
     }
-
-    @Test
-    fun shouldReturnNumber_whenInputIsSingleNumberInString() {
-        val calculator = Calculator()
-        assertEquals(1, calculator.add("1"))
-        assertEquals(10, calculator.add("10"))
-        assertNotEquals(10, calculator.add("5"))
-    }
-
-    @Test
-    fun shouldParseToNumberList_whenDelimiterIsComma() {
-        val calculator = Calculator()
-        assertEquals(listOf(1,2,3), calculator.parse("1,2,3"))
-        assertEquals(listOf(123,456,789,1,2,3), calculator.parse("123,456,789,1,2,3"))
-    }
-
-    @Test
-    fun shouldReturnSum_whenInputIsNumberSeparatedByComma() {
-        val calculator = Calculator()
-        assertEquals(3, calculator.add("1,2"))
-        assertEquals(5, calculator.add("2,3"))
-        assertEquals(12, calculator.add("2,4,6"))
-        assertEquals(115, calculator.add("1,2,3,4,5,100"))
-        assertNotEquals(10, calculator.add("1,2,3"))
-    }
-
-    @Test
-    fun shouldParseToNumberList_whenDelimiterIsCommaOrNewline() {
-        val calculator = Calculator()
-        assertEquals(listOf(1,2,3), calculator.parse("1,2\n3"))
-
-    }
-
-    @Test
-    fun shouldThrowIllegalArgumentException_whenDelimiterIsNotCommaOrNewLine() {
-        val calculator = Calculator()
-        Assertions.assertThrows(IllegalArgumentException::class.java) {
-            calculator.parse("1\\2\\3")
-        }
-    }
-
-    @Test
-    fun shouldThrowIllegalArgumentException_whenExpressionHasConsecutiveDelimiters() {
-        val calculator = Calculator()
-        Assertions.assertThrows(IllegalArgumentException::class.java) {
-            calculator.parse("1,2,\n3")
-        }
-    }
-
-    @Test
-    fun shouldThrowIllegalArgumentException_whenExpressionEndsWithDelimiter() {
-        val calculator = Calculator()
-        Assertions.assertThrows(IllegalArgumentException::class.java) {
-            calculator.parse("1,2,3,")
-        }
-        Assertions.assertThrows(IllegalArgumentException::class.java) {
-            calculator.parse("1\n2,3\n")
-        }
-    }
-
 }
