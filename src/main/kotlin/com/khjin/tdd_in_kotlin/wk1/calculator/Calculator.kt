788a1693ff2b58701f22d427fa9ee9db57a35feb
horilla <khjin1197@gmail.com>
2024-02-21 01:54:13 +0900

    Kata 2 - case 2 : if expression contains only one number, return it

--- src/main/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/Calculator.kt
+++ src/main/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/Calculator.kt
@@ -3,7 +3,11 @@ package org.example.com.khjin.tdd_in_kotlin.wk1.calculator
 class Calculator {
 
     fun add(expression: String): Int{
-        return 0
+        if(expression.isEmpty()){
+            return 0
+        }else{
+            return expression.toInt()
+        }
     }
 
 }
--- src/test/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/khjin/tdd_in_kotlin/wk1/calculator/CalculatorTest.kt
@@ -3,6 +3,7 @@ package com.khjin.tdd_in_kotlin.wk1.calculator
 import org.example.com.khjin.tdd_in_kotlin.wk1.calculator.Calculator
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class CalculatorTest {
 
@@ -10,5 +11,14 @@ class CalculatorTest {
     fun shouldReturnZero_whenExpressionIsEmpty() {
         val calculator = Calculator()
         assertEquals(0, calculator.add(""))
+        assertNotEquals(123, calculator.add(""))
+    }
+
+    @Test
+    fun shouldReturnNumber_whenExpressionHasOnlyOneNumber() {
+        val calculator = Calculator()
+        assertEquals(1, calculator.add("1"))
+        assertEquals(999, calculator.add("999"))
+        assertNotEquals(0, calculator.add("1"))
     }
 }
